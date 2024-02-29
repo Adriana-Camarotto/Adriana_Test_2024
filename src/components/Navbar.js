291da16347f7f812a4529c14627356ca4ac597ff
Adriana Camarotto <adriana.camarotto@gmail.com>
2024-02-29 18:04:38 +0000

    added last links and login button to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   const toggleNav = (event) => {
     event.preventDefault();
@@ -10,10 +10,9 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-transparent  text-primary-color absolute z-20 w-full">
+    <nav className="bg-transparent absolute z-20 w-full">
       <div className="container mx-auto px-4">
         <div className="flex flex-col lg:flex-row items-start justify-between lg:py-4 md:py-8 py-12">
-          
           <button
             className="text-custom-brown focus:outline-none lg:hidden"
             onClick={toggleNav}
@@ -33,24 +32,64 @@ export default function Navbar() {
               ></path>
             </svg>
           </button>
-          <div className={`lg:flex lg:flex-grow lg:justify-start lg:items-center ${isNavOpen ? 'block' : 'hidden'}`}>
-            <ul className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 text-primary-color text-xs">
+
+          <div
+            className={`lg:flex lg:flex-grow px-5 lg:justify-start lg:items-center ${
+              isNavOpen ? "block" : "hidden"
+            }`}
+          >
+            <ul className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-5 text-primary-color text-sm">
               <li>
-                <Link to="/" className="hover:text-custom-brown ">Home</Link>
+                <Link to="/" className="hover:text-custom-brown ">
+                  Home
+                </Link>
               </li>
               <li>
-                <Link to="/about" className="hover:text-custom-brown ">About</Link>
+                <Link to="/about" className="hover:text-custom-brown ">
+                  About
+                </Link>
               </li>
               <li>
-                <Link to="/ourArtists" className="hover:text-custom-brown ">Our Artists</Link>
+                <Link to="/ourArtists" className="hover:text-custom-brown ">
+                  Our Artists
+                </Link>
               </li>
               <li>
-                <Link to="/whereToBy" className="hover:text-custom-brown ">Where to Buy</Link>
+                <Link to="/whereToBy" className="hover:text-custom-brown ">
+                  Where to Buy
+                </Link>
               </li>
               <li>
-                <Link to="./Blog/Page" className="hover:text-custom-brown ">Blog</Link>
+                <Link to="./Blog/Page" className="hover:text-custom-brown ">
+                  Blog
+                </Link>
               </li>
             </ul>
+
+            <div className="lg:flex lg:flex-grow lg:items-center lg:justify-end ">
+              <ul className="flex space-x-4flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-5 text-primary-color text-sm">
+                <li>
+                  <Link
+                    to="./artistSubmissions"
+                    className="hover:text-custom-brown"
+                  >
+                    Artist Submissions
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="./tradeEnquiries"
+                    className="hover:text-custom-brown mr-5"
+                  >
+                    Trade Enquiries
+                  </Link>
+                </li>
+              </ul>
+
+              <button className="bg-custom-brown  text-primary-color text-sm p-2">
+                Arthub Login
+              </button>
+            </div>
           </div>
         </div>
       </div>
